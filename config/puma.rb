b9e52539ba6764de6d5e873cde347a87cec9e9df
markwinkler <mark@impekable.com>
2021-02-01 15:35:57 -0600

    Puma config uses 5 threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,19 @@
 rails_env = ENV['RAILS_ENV'] || "development"
 app_name = 'mobile-api'
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 
 if rails_env == 'development'
-  threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
   threads threads_count, threads_count
   port        ENV.fetch("PORT") { 3000 }
   environment ENV.fetch("RAILS_ENV") { "development" }
 else
-  threads 1, 6
+  threads threads_count, threads_count
   environment rails_env
-  app_dir = File.expand_path("../..", __FILE__)
   shared_dir = "/home/deploy/apps/#{app_name}/shared"
 
   workers 2
+  preload_app!
+
   # Set up socket location
   bind "unix://#{shared_dir}/sockets/puma.sock"
 
@@ -22,12 +23,10 @@ else
   # Set master PID and state locations
   pidfile "#{shared_dir}/pids/puma.pid"
   state_path "#{shared_dir}/pids/puma.state"
-  activate_control_app
+  #activate_control_app
 
   on_worker_boot do
-    require "active_record"
-    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
   end
 end
 
